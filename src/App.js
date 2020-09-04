e28ebae4ba4a9c1034f2fe2c66119f8baf305f00
Kishan Maurya <kkmaurya.0095@gmail.com>
2020-09-04 07:49:32 +0530

    Audio recortding and Playback feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MicRecorder from 'mic-recorder-to-mp3';
+import { Base64 } from 'js-base64';
+import Button from '@material-ui/core/Button';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic';
+import MicOffIcon from '@material-ui/icons/MicOff';
+import Container from '@material-ui/core/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const main={
+  marginBottom:'20px'
 }
+const record = {
+  position:'relative',
+  right:'60px',
+  borderRadius:'50px'
+}
+const stop={
+  position:'relative',
+  left:'60px',
+  borderRadius:'50px'
+}
+
+const Mp3Recorder = new MicRecorder({ 
+    bitRate: 64 ,
+    prefix: "data:audio/wav;base64,",
+});
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isRecording: false,
+      blobURL: '',
+      isBlocked: false,
+    };
+  }
+
+
+  start = () => {
+    if (this.state.isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      Mp3Recorder
+        .start()
+        .then(() => {
+          this.setState({ isRecording: true });
+        }).catch((e) => console.error(e));
+    }
+  };
+
+  stop = () => {
+    Mp3Recorder
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const blobURL = URL.createObjectURL(blob)
+        const binaryString = btoa(blobURL)
+        console.log(binaryString);
+        this.setState({ blobURL, isRecording: false });
+      }).catch((e) => console.log(e));
+  };
+
+  componentDidMount() {
+    navigator.getUserMedia({ audio: true },
+      () => {
+        console.log('Permission Granted');
+        this.setState({ isBlocked: false });
+      },
+      () => {
+        console.log('Permission Denied');
+        this.setState({ isBlocked: true })
+      },
+    );
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+      
+        <header className="App-header">
+        {/* <Button variant="contained" color="primary" >Start When ready</Button> */}
+          <Container maxWidth="sm" style={main}>
+            <Button variant="contained" style={record} color="primary"  onClick={this.start} disabled={this.state.isRecording}><MicIcon/></Button>
+            <Button variant="contained" style={stop} color="primary" onClick={this.stop} disabled={!this.state.isRecording}><MicOffIcon /></Button>
+          </Container>
+          <audio src={this.state.blobURL} controls="controls" />
+        </header>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
---
+++ src/dist/App.dev.js
@@ -0,0 +1,49 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./App.css");
+
+var _micRecorderToMp = _interopRequireDefault(require("mic-recorder-to-mp3"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Mp3Recorder = new MicRecorder({
+  bitRate: 128
+});
+
+var App =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
+  }
+
+  return App;
+}(_react["default"].Component);
+
+var _default = App;
+exports["default"] = _default;
